ba200f2db1bda5b0b80e54d79529a64602c0ffbb
Anubhav Patel <anubhavpatel6391@gmail.com>
2021-09-25 23:38:33 +0530

    project start

--- app/src/main/java/com/example/mass/PracticeActivity.kt
+++ app/src/main/java/com/example/mass/PracticeActivity.kt
@@ -1,11 +1,40 @@
 package com.example.mass
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import de.hdodenhof.circleimageview.CircleImageView
 
 class PracticeActivity : AppCompatActivity() {
+    private lateinit var profile_image : CircleImageView
+    private lateinit var camBtn : Button
+    private var filepath: Uri?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_practice)
+        profile_image=findViewById(R.id.profile_image)
+        camBtn=findViewById(R.id.camBtn)
+        profile_image.setOnClickListener {
+            startFileChooser()
+        }
+    }
+    private fun startFileChooser() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Choose Image"), 111)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 111 && resultCode == Activity.RESULT_OK && data != null) {
+            filepath = data.data!!
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, filepath)
+            profile_image.setImageBitmap(bitmap)
+        }
     }
 }
---
+++ app/src/main/java/com/example/mass/SongUi.kt
@@ -0,0 +1,11 @@
+package com.example.mass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SongUi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_song_ui)
+    }
+}
