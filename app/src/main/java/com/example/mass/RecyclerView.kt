ff7701cb2b23547eb03430e8d0d7bbf95d29e62f
anubhavpatel <anubhavpatel6391@gmail.com>
2021-11-22 22:35:28 +0530

    added recycler view

---
+++ app/src/main/java/com/example/mass/RecyclerView.kt
@@ -0,0 +1,12 @@
+package com.example.mass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecyclerView : AppCompatActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler_view)
+    }
+}
--- app/src/main/java/com/example/mass/RegisterActivity.kt
+++ app/src/main/java/com/example/mass/RegisterActivity.kt
@@ -7,6 +7,7 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
         val actionbar =supportActionBar
         actionbar!!.title="Register"
         actionbar.setDisplayHomeAsUpEnabled(true)
